f27205227015778962a1af9acd46b0515294c12d
Uboatwaffe <mpiatek1512@gmail.com>
2023-11-10 19:43:06 +0100

    Homework ui
    Signed-off-by: Uboatwaffe <mpiatek1512@gmail.com>

--- UI/src/main/java/org/ui/program/Tasks.java
+++ UI/src/main/java/org/ui/program/Tasks.java
@@ -21,9 +21,9 @@ public class Tasks extends JFrame {
     private JPanel homeworks;
     private JPanel other;
     private JLabel info;
-    private JList subject_list;
-    private JList date_list;
-    private JList scale_list;
+    private JList<String> subject_list;
+    private JList<String> date_list;
+    private JList<Integer> scale_list;
     private JLabel subject;
     private JLabel date;
     private JButton closeButton;
@@ -34,6 +34,15 @@ public class Tasks extends JFrame {
     private JScrollPane scale_scroll;
     private JPanel adding;
     private JPanel deleting;
+    private JLabel info_hw;
+    private JScrollPane subject_scroll_hw;
+    private JScrollPane scale_scroll_hw;
+    private JList<String> subject_list_hw;
+    private JList<Integer> scale_list_hw;
+    private JButton closeButtonHW;
+    private JButton refreshButtonHW;
+    private JLabel subject_hw;
+    private JLabel scale_hw;
 
     public Tasks() {
         setContentPane(panel);
@@ -43,6 +52,19 @@ public class Tasks extends JFrame {
         setVisible(true);
         setResizable(Properties.getScalable());
         closeButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispatchEvent(new WindowEvent(Tasks.this, WindowEvent.WINDOW_CLOSING));
+                SwingUtilities.invokeLater(org.ui.Main::new);
+            }
+        });
+
+        closeButtonHW.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
              *
