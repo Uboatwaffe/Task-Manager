8f260723db9fbdf8261653b313133c13c18879e4
Uboatwaffe <mpiatek1512@gmail.com>
2023-11-10 17:57:41 +0100

    Structure
    Signed-off-by: Uboatwaffe <mpiatek1512@gmail.com>

---
+++ UI/src/main/java/org/ui/Main.java
@@ -0,0 +1,14 @@
+package org.ui;
+
+/**
+ * Main class
+ *
+ * @author Maciej
+ * @version 0.1
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/org/task_manager/Main.java
+++
@@ -1,7 +0,0 @@
-package org.task_manager;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
